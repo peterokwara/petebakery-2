c59a8c58c8cbee39f98f8943763cea60514c8ac2
Peter Okwara <peter.okwara@outlook.com>
2020-10-25 16:46:39 +0300

    Reset, remove things dealing with locale

--- app.js
+++ app.js
@@ -32,35 +32,85 @@ app.get("/webhook", (req, res) => {
     }
 });
 
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {
-
+// Creates the endpoint for your webhook
+app.post("/webhook", (req, res) => {
     let body = req.body;
 
     // Checks if this is an event from a page subscription
     if (body.object === "page") {
-
-
         // Returns a '200 OK' response to all requests
         res.status(200).send("EVENT_RECEIVED");
 
+        // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function (entry) {
+            if ("changes" in entry) {
+                // Handle Page Changes event
+                let receiveMessage = new Receive();
+                if (entry.changes[0].field === "feed") {
+                    let change = entry.changes[0].value;
+                    switch (change.item) {
+                        case "post":
+                            return receiveMessage.handlePrivateReply(
+                                "post_id",
+                                change.post_id
+                            );
+                        case "comment":
+                            return receiveMessage.handlePrivateReply(
+                                "comment_id",
+                                change.comment_id
+                            );
+                        default:
+                            console.log("Unsupported feed change type.");
+                            return;
+                    }
+                }
+            }
+
             // Gets the body of the webhook event
             let webhookEvent = entry.messaging[0];
+            // console.log(webhookEvent);
+
+            // Discard uninteresting events
+            if ("read" in webhookEvent) {
+                // console.log("Got a read event");
+                return;
+            }
+
+            if ("delivery" in webhookEvent) {
+                // console.log("Got a delivery event");
+                return;
+            }
 
             // Get the sender PSID
             let senderPsid = webhookEvent.sender.id;
-            users[senderPsid] = user;
 
-            let receiveMessage = new Receive(users[senderPsid], webhookEvent);
-            return receiveMessage.handleMessage();
+            if (!(senderPsid in users)) {
+                let user = new User(senderPsid);
 
-        })
+                GraphAPi.getUserProfile(senderPsid)
+                    .then(userProfile => {
+                        user.setProfile(userProfile);
+                    })
+                    .catch(error => {
+                        // The profile is unavailable
+                        console.log("Profile is unavailable:", error);
+                    })
+                    .finally(() => {
+                        users[senderPsid] = user;
+                        let receiveMessage = new Receive(users[senderPsid], webhookEvent);
+                        return receiveMessage.handleMessage();
+                    });
+            } else {
+                let receiveMessage = new Receive(users[senderPsid], webhookEvent);
+                return receiveMessage.handleMessage();
+            }
+        });
     } else {
-        res.sendStatus(404)
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
     }
-
 });
+
 
 // Verify that the callback came from Facebook.
 function verifyRequestSignature(req, res, buf) {
